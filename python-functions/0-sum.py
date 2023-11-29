0c3bbc3cec4fddc442a76ab6b5aef4a95849465f
wslmu <wissalmoujann@gmail.com>
2023-11-29 23:52:47 +0100

    functions

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,5 +1,11 @@
 def add(a, b):
     return a + b
 
-result = add(3, 6)
+result = add(1, 2)
+print(result)
+result = add(100, -2)
+print(result)
+result = add(-100, -2)
+print(result)
+result = add(0, 0)
 print(result)
