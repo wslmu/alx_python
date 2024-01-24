d8c52cde462960f1b9654029c6ba532866863ee7
wslmu <wissalmoujann@gmail.com>
2024-01-24 19:46:23 +0100

    k

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -1,35 +1,22 @@
-'''
-  By here, we are  importing the Flask class from the flask web framework. 
-  Flask is a micro web framework for Python that simplifies the process of building web applications. 
-  The Flask class is a core component of Flask and is used to create a web application instance.
-
-'''
- 
+#!/usr/bin/python3
+'''Importing Flask form flask'''
 from flask import Flask
-
-app = Flask(__name__)
-
-
-
-from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def Hello_HBNB():
-    return 'Hello HBNB!'
-
+def hello_hbnb():
+    return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
-def HBNB():
-    return 'HBNB'
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return 'C {}'.format((text).replace("_", " "))
 
 
- 
-
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
